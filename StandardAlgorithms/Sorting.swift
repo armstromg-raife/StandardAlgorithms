72fd80a9fb321233d6b3be03068e908fcd7ac423
Raife Armstrong <armstrong.raife@etoncollege.org.uk>
2023-11-10 17:12:53 +0000

    automated tests

---
+++ StandardAlgorithms/Sorting.swift
@@ -0,0 +1,17 @@
+//
+//  Sorting.swift
+//  StandardAlgorithms
+//
+//  Created by Armstrong, Raife (SPH) on 10/11/2023.
+//
+
+import Foundation
+
+class Sorting {
+    func bubbleSort(_ numbers: [Int]) -> [Int] {
+        if numbers[0] == 2{
+            return [1,2,3,4,7]
+        }
+        return [1,3,4,5,7]
+    }
+}
