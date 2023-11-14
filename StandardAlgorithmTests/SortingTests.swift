103489212f3338a5551b1d2b733ea78f92074033
Raife Armstrong <armstrong.raife@etoncollege.org.uk>
2023-11-14 10:47:04 +0000

    integrated merge sort EW started Quick sort

--- StandardAlgorithmTests/SortingTests.swift
+++ StandardAlgorithmTests/SortingTests.swift
@@ -21,4 +21,43 @@ final class SortingTests: XCTestCase {
         }
         //assert
     }
+    
+    func testCheckingWeatherTheMergeSortReturnsTheSortedListFromGreatestToSmallest() {
+            //arrange
+        let testCases = [(data:[38, 27, 43, 3, 9, 82, 10], expected:[3, 9, 10, 27, 38, 43, 82]),(data:[5, 2, 7, 1, 8, 4, 3, 6], expected:[1, 2, 3, 4, 5, 6, 7, 8]),(data:[9, 1, 5, 3, 7, 2, 8, 4, 6, 10], expected:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])]
+                //act
+
+
+            for testCase in testCases {
+                let result: ([Int]) = mergeSort(array: testCase.data)
+                XCTAssertEqual(result, testCase.expected)
+            }
+        }
+
+
+    func testMergeSortSpeed()  {
+        
+        self.measure {
+            let testCases = [([38, 27, 43, 3, 9, 82, 10], [3, 9, 10, 27, 38, 43, 82]),([5, 2, 7, 1, 8, 4, 3, 6], [1, 2, 3, 4, 5, 6, 7, 8]),([9, 1, 5, 3, 7, 2, 8, 4, 6, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])]
+            //act
+
+            for testCase in testCases {
+                let result = mergeSort(array: testCase.0)
+            }
+        }
+    }
+
+    
+    func testQuickSortWithManyShuffledArraysOfIntegersRerurnsArrayInAscendingOrder(){
+        //arrange
+        let sorting = Sorting()
+        
+        let testCases = [(data:[5,1,3,7,4],expected: [1,3,4,5,7]),(data:[2,1,3,7,4],expected: [1,2,3,4,7]),(data:[7,4],expected: [4,7])]
+        //act
+        for testCase in testCases{
+            let actual = sorting.quickSort(testCase.data)
+            XCTAssertEqual(actual, testCase.expected)
+        }
+        //assert
+    }
 }
