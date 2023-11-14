28206e66e31e6d81770fded5d1672d3f7f020a1a
Raife Armstrong <armstrong.raife@etoncollege.org.uk>
2023-11-14 10:47:04 +0000

    integrated merge sort EW started Quick sort

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -14,4 +14,74 @@ class Sorting {
         }
         return [1,3,4,5,7]
     }
+    
+    func mergeSort(array : [Int]) -> [Int]{
+        if array.count>1{
+            let mid=array.count/2
+            let leftArray: ([Int]) = Array(array[0..<mid])
+            let rightArray: ([Int]) = Array(array[mid..<array.count])
+            
+            let sortedLeft = mergeSort(array: leftArray)
+            let sortedRight = mergeSort(array: rightArray)
+            
+            return merge(sortedLeft,sortedRight)
+        }else{
+            return array
+        }
+    }
+
+
+    func merge(_ leftArray: [Int],_ rightArray: [Int]) -> [Int]{
+        var output: [Int] = []
+        var leftIndex = 0
+        var rightIndex = 0
+        
+        while leftIndex < leftArray.count && rightIndex < rightArray.count {
+            let leftNum = leftArray[leftIndex]
+            let rightNum = rightArray[rightIndex]
+            
+            if leftNum < rightNum {
+                output.append(leftNum)
+                leftIndex += 1
+            } else {
+                output.append(rightNum)
+                rightIndex += 1
+            }
+            
+        }
+        if leftIndex < leftArray.count{
+            output.append(contentsOf: leftArray[leftIndex...])
+        }else{
+            output.append(contentsOf: rightArray[rightIndex...])
+        }
+        return output
+    }
+
+    
+    func quickSort(_ numbers: [Int]) -> [Int] {
+        let pivot = numbers[0]
+        var left: [Int] = [Int]()
+        var right: [Int] = [Int]()
+        var output: [Int] = [Int]()
+        
+        for i in 1..<numbers.count{
+            if numbers[i]<pivot{
+                left.append(numbers[i])
+            }else{
+                right.append(numbers[i])
+            }
+        }
+        if left.count>1{
+            return quickSort(left)
+        }else{
+            return left
+        }
+        if right.count>1{
+            return quickSort(right)
+        }else{
+            return right
+        }
+        output
+        return output
+    }
 }
